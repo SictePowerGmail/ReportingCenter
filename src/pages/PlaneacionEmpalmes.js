5befa9eebf409fd14b1f7102b5c2b7c1dddff991
SictePowerGmail <SictePowerGmail@gmail.com>
2022-10-12 15:53:33 -0500

    Seguridad empalmes

--- src/pages/PlaneacionEmpalmes.js
+++ src/pages/PlaneacionEmpalmes.js
@@ -6,7 +6,7 @@ export default class Capacidades extends Component {
     return (
       <PowerBi
       title="Empalmes"
-      src="https://app.powerbi.com/view?r=eyJrIjoiMzM1NjU2YzYtYTI2MS00MWExLWI4YjAtMGFhOTMyNWQ1OWFiIiwidCI6ImUwYmZlOTBkLTIwZTAtNDEwYi1iNmYxLTQyOWIwNDNkMzYwOCJ9&pageName=ReportSection6f0d973d70d0ab284590">
+      src="https://app.powerbi.com/reportEmbed?reportId=34b80d2b-bf64-4be8-b2c9-472d60cb2ac2&autoAuth=true&ctid=e0bfe90d-20e0-410b-b6f1-429b043d3608">
       </PowerBi>
     )
   }
