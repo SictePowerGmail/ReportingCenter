ddf10f1c270f07a4a3ebac11a693af87fdd0097c
SictePowerGmail <SictePowerGmail@gmail.com>
2022-12-27 12:01:13 -0500

    Tablero de seguimiento nuevamente

--- src/pages/Seguimiento.js
+++ src/pages/Seguimiento.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { PowerBiPrivate } from '../components/Navbar/Narbar.elements'
 
-export default class PBRB_SeguimientoEmpalmes extends Component {
+export default class Seguimiento extends Component {
   render() {
     return (           
       <PowerBiPrivate
       title="Seguimiento empalmes"
-      src="https://app.powerbi.com/rdlEmbed?reportId=027abcdf-b719-4d1e-ba34-468e5bc71aa8&autoAuth=true&ctid=e0bfe90d-20e0-410b-b6f1-429b043d3608">
+      src="https://app.powerbi.com/reportEmbed?reportId=1875abf9-817c-4164-a0e7-0685547a88a9&autoAuth=true&ctid=e0bfe90d-20e0-410b-b6f1-429b043d3608">
       </PowerBiPrivate>
     )
   }
