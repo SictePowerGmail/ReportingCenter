5c1a3f9c6e5d8993ea126db81514513c8fd1fe1f
BryanUtria <brayan.kastel@gmail.com>
2025-04-25 12:45:48 -0500

    se corrige parametro que se requiere enviar por el enlace de capacidades

--- src/pages/reportes/capacidades/Capacidades.js
+++ src/pages/reportes/capacidades/Capacidades.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../../powerbi.css'
+import Cookies from 'js-cookie';
 
 function Capacidades() {
+
+  const role = Cookies.get('userRole');
 
   return (
     <div className='powerbi'>
       <iframe
         title="Capacidades"
-        src="https://bryansicte.github.io/Sicte-SAS-Capacidades/#/Principal"
+        src={`https://bryansicte.github.io/Sicte-SAS-Capacidades/#/Principal?role=${encodeURIComponent(role)}`}
       ></iframe>
     </div>
   );
