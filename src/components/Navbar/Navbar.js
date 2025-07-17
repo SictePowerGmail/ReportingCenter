b92f431e008a6b2038a8086f0a61096d4c14094d
BryanUtria <brayan.kastel@gmail.com>
2025-07-17 16:44:31 -0500

    Se termina de hacer el formulario de enel ambiental para la app de supervision

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -844,7 +844,7 @@ function Navbar() {
                                 </div>
                             </li>
 
-                            <span className={`sub-titulo ${showMobileMenu ? 'abierto' : 'cerrado'}`}>Version 2.0.10</span>
+                            <span className={`sub-titulo ${showMobileMenu ? 'abierto' : 'cerrado'}`}>Version 2.0.11</span>
                         </ul>
 
                         {/* <div className='Logo2'>
