8885bf4803f72f4ea72c16c9f518a33a34493395
BryanUtria <brayan.kastel@gmail.com>
2025-09-19 15:37:08 -0500

    Se cambia el link de productividad enel AP

--- src/pages/produccion/EnelAp.js
+++ src/pages/produccion/EnelAp.js
@@ -7,7 +7,7 @@ function EnelAp() {
     <div className='powerbi'>
       <iframe
         title="Enel Alumbrado Publico"
-        src="https://app.powerbi.com/view?r=eyJrIjoiMWVmMDQwMDAtMDgzMC00YjdhLWI2NjgtYjE3OTk2NGNlZWExIiwidCI6ImUwYmZlOTBkLTIwZTAtNDEwYi1iNmYxLTQyOWIwNDNkMzYwOCJ9"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMzkzNTIyNmItMTMzNi00MGUxLWExYmItNzRkNjJiYmFjNzQ4IiwidCI6ImUwYmZlOTBkLTIwZTAtNDEwYi1iNmYxLTQyOWIwNDNkMzYwOCJ9"
       ></iframe>
     </div>
   );
