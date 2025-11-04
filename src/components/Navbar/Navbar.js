92bc407720fd758989bc0eeeb06d50ee020f54de
Brayan Steven Castelblanco Utria <brayan.kastel@gmail.com>
2025-11-04 12:30:59 -0500

    Se agrega nueva encuesta al aplicativo de encuestas v3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -870,7 +870,7 @@ function Navbar() {
                                 </div>
                             </li>
 
-                            <span className={`sub-titulo ${showMobileMenu ? 'abierto' : 'cerrado'}`}>Version 2.0.63</span>
+                            <span className={`sub-titulo ${showMobileMenu ? 'abierto' : 'cerrado'}`}>Version 2.0.64</span>
                         </ul>
 
                         {/* <div className='Logo2'>
